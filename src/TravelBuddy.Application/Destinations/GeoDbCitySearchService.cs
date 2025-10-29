7fa5d19e2cdf58919ca5ca9bcd597f8cdfd3aaff
gomezagustin332 <122643040+gomezagustin332@users.noreply.github.com>
2025-10-28 23:03:03 -0300

    Revert "Revert "Implementar operación 31 buscar ciudades por nombre""

---
+++ src/TravelBuddy.Application/Destinations/GeoDbCitySearchService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using TravelBuddy.Destinations;
+namespace TravelBuddy.Destinations
+{
+    public class GeoDbCitySearchService : ICitySearchService
+    {
+        private static readonly string apiKey = "1b87288382msh04081de1250362fp1acf94jsn6c66e7e31d14";
+        private static readonly string baseUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+        public async Task<CitySearchResultDto> SearchCities(CitySearchRequestDTO request)
+        {
+            using var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Key", apiKey);
+            httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Host", "wft-geo-db.p.rapidapi.com");
+
+            string url = $"{baseUrl}/cities?namePrefix={Uri.EscapeDataString(request.PartialName)}&limit=10&sort=population";
+
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadFromJsonAsync<GeoDbResponse>();
+                var cities = json.Data.Select(c => new CityDto
+                {
+                    Name = c.City ?? string.Empty,
+                    Country = c.Country ?? string.Empty,
+                    Population= c.Population ?? 0,
+                    Latitude = c.Latitude,
+                    Longitude = c.Longitude
+
+                }).ToList();
+                return new CitySearchResultDto { Cities = cities };
+            }
+            else
+            {
+                 throw new Exception($"Error fetching cities: {response.ReasonPhrase}");
+            }
+
+        }
+
+    }
+}
+
+
---
+++ src/TravelBuddy.Application/Destinations/GeoDbResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBuddy.Destinations
+{
+    class GeoDbResponse
+    {
+        public List<GeoDbCity> Data { get; set; } = new();
+    }
+}
