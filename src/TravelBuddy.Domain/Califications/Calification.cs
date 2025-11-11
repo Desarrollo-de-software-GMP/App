52e70c219e0e370cf472e019897ac3fc0b195659
Mateo Mista <mateomista5@gmail.com>
2025-11-11 16:27:58 -0300

    agregar entidad calificacion

---
+++ src/TravelBuddy.Domain/Califications/Calification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using TravelBuddy.Coordenadas;
+
+namespace TravelBuddy.Califications
+{
+    internal class Calification : AuditedAggregateRoot<Guid>
+    {
+        public required DateTime createdDate { get; set; }
+        public required DateTime updatedDate { get; set; }
+        public required int punctuation { get; set; }
+        public string ? comment { get; set; }
+    }
+}
